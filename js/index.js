38dfc286a670f5290b97e9d2cc3fabb05bc66d0f
qaoleg-3107 <qaoleg31@gmail.com>
2019-09-01 18:45:20 +0000

    2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-alert('Hello JavaScript');
+let message = 'Hello JavaScript!';
+alert(message);
+message = 'Nice to see you!';
+alert(message);
+
+const userAge = parseInt(prompt('Enter you age'));
+ alert('Your age is ' + userAge);
+
+const nextAge = addOne(userAge);
+alert('Your age in next year is ' + nextAge);
+
+if (userAge >= 18 && userAge < 65) {
+alert('You are adult')
+} else if (userAge >= 65) {
+alert ('You are mature')
+} else {
+alert('You are child')
+}
+
+function addOne(userA) {
+  const newValue = userA + 1;
+return newValue;
+}
